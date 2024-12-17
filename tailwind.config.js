591b3016c5136c352a91862cadc11079544b0fbb
KiBohr <katharinabohr@web.de>
2024-12-17 17:02:36 +0100

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+    daisyui: {
+    themes: [
+        {
+        kbfinery: {
+            "primary": "#5b81da",
+            "secondary": "#b1a737",
+            "accent": "#e33839",
+            "neutral": "#b2d5da",
+            "base-100": "#fcffff",
+            "info": "#efdfc2",
+            "success": "#e1dd3d",
+            "warning": "#4d72dd",
+            "error": "#ff0000",
+            }
+        }
+    ]
+    }
+}
